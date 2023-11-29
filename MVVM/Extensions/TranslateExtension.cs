d061be7ce1c4441616279f59784937e2c25b695f
bg <brahim.guerbouj@outlook.fr>
2023-11-29 11:07:00 +0100

    Localization  Part 2

---
+++ MVVM/Extensions/TranslateExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using MVVM.Resources.Languages;
+
+namespace MVVM.Extensions
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+
+        public TranslateExtension()
+        {
+            AppResources.Culture = CultureInfo.CurrentCulture;
+        }
+        public string Text { get; set; }
+
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(Text))
+            {
+                return "";
+            }
+
+            try
+            {
+                var ci = Thread.CurrentThread.CurrentCulture;
+                var translation = AppResources.ResourceManager.GetObject(Text, ci);
+                if (translation == null)
+                {
+                    return Text;
+                }
+
+                return translation;
+            }
+            catch (Exception ex)
+            {
+                return Text;
+            }
+        }
+    }
+}
+
